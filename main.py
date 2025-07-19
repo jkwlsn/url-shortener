d92365fdcc15686ec6a5af4dec2153dfb033da88
Jake Wilson <hi@jkwlsn.dev>
2025-07-19 15:40:12 +0100

    Creates connection string to database
    Following examples in SQLAlchemy documentation.
    
    The construction of the `DATABASE_URL` follows the pattern I used in a previous project

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-import os
 
 from dotenv import dotenv_values
 
-SETTINGS: dict[str, str | None] = {**dotenv_values(".env")}
+"""Get settings from .env"""
+SETTINGS: dict[str, str | None] = {**dotenv_values(dotenv_path=".env")}
+
+"""Construct database connection string"""
+db: dict[str, str | None] = {
+    "host": SETTINGS["POSTGRES_HOST"],
+    "port": SETTINGS["POSTGRES_PORT"],
+    "user": SETTINGS["POSTGRES_USER"],
+    "password": SETTINGS["POSTGRES_PASSWORD"],
+    "dbname": SETTINGS["POSTGRES_DB"],
+}
+
+DATABASE_URL: str = f"postgresql+psycopg_async://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['dbname']}"
 
 
 def main() -> None:
