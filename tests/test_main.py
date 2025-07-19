bb0f4e1a101e265e98c4607af3602554fbe29563
Jake Wilson <hi@jkwlsn.dev>
2025-07-19 22:27:52 +0100

    Adds valid and invalid tests for get_long_url

--- tests/test_main.py
+++ tests/test_main.py
@@ -232,3 +232,30 @@ class TestMain:
 
         assert response.status_code == 422
         assert "long_url" in response.text
+
+    @pytest.mark.asyncio
+    @patch("main.get_long_url", new_callable=AsyncMock)
+    async def test_return_long_url_for_valid_slug(
+        self, mock_get_long_url: MagicMock
+    ) -> None:
+        mock_get_long_url.return_value = "https://www.example.com/page"
+        async with AsyncClient(
+            transport=ASGITransport(app=app), base_url="http://test"
+        ) as ac:
+            response = await ac.get(url="/ABCD123")
+
+        assert response.status_code == 307
+        assert response.is_redirect
+
+    @pytest.mark.asyncio
+    @patch("main.get_long_url", new_callable=AsyncMock)
+    async def test_return_error_for_invalid_slug(
+        self, mock_get_long_url: MagicMock
+    ) -> None:
+        mock_get_long_url.side_effect = NoMatchingSlugError("No matching slug found")
+        async with AsyncClient(
+            transport=ASGITransport(app=app), base_url="http://test"
+        ) as ac:
+            response = await ac.get(url="/ABCD123")
+
+        assert response.status_code == 404
