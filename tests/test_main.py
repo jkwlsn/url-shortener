109073a8c8cf7396f7b445c8006478e1bc3bd574
Jake Wilson <hi@jkwlsn.dev>
2025-07-19 17:11:56 +0100

    Adds tests for pydantic models
    There are four models defined in the tests: LongUrlAccept, LongUrlReturn, SlugAccept, ShortUrlReturn.
    
    These represent the data that will be handled by the routes.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,15 @@
 import pytest
+from pydantic import HttpUrl, ValidationError
 from sqlalchemy import text
 
-from main import async_session, main
+from main import (
+    LongUrlAccept,
+    LongUrlReturn,
+    ShortUrlReturn,
+    SlugAccept,
+    async_session,
+    main,
+)
 
 
 class TestMain:
@@ -17,3 +25,87 @@ class TestMain:
             assert value == 1, (
                 "Database connection failed or returned unexpected result"
             )
+
+    """Test suite for Pydantic schemas"""
+
+    def test_accept_valid_long_urls(self) -> None:
+        """Valid urls should be accepted"""
+        data: dict[str, str] = {
+            "long_url": "https://example.com/a/deep/page/and-some-more-information-here.html"
+        }
+
+        schema: LongUrlAccept = LongUrlAccept(**data)
+
+        assert isinstance(schema.long_url, HttpUrl)
+        assert (
+            str(schema.long_url)
+            == "https://example.com/a/deep/page/and-some-more-information-here.html"
+        )
+
+    def test_do_not_accept_invalid_long_urls(self) -> None:
+        """Invalid urls should raise ValidationErrors"""
+        invalid_data: dict[str, str] = {"long_url": "an-invalid-url"}
+
+        with pytest.raises(ValidationError) as e:
+            schema: LongUrlAccept = LongUrlAccept(**invalid_data)
+
+        assert "validation error" in str(e.value)
+
+    def test_return_valid_long_urls(self) -> None:
+        """Application should return valid URLs"""
+        data: dict[str, str] = {
+            "long_url": "https://example.com/a/deep/page/and-some-more-information-here.html"
+        }
+
+        schema: LongUrlReturn = LongUrlReturn(**data)
+
+        assert isinstance(schema.long_url, HttpUrl)
+        assert (
+            str(schema.long_url)
+            == "https://example.com/a/deep/page/and-some-more-information-here.html"
+        )
+
+    def test_do_not_return_invalid_long_urls(self) -> None:
+        """Invalid short urls should not be returned"""
+        invalid_data: dict = {"long_url": "an-invalid-url"}
+
+        with pytest.raises(ValidationError) as e:
+            LongUrlReturn(**invalid_data)
+
+        assert "validation error" in str(e.value)
+
+    def test_accept_valid_slugs(self) -> None:
+        """Valid slugs should be valid"""
+        data: dict[str, str] = {"slug": "A1b2C3d"}
+
+        schema: SlugAccept = SlugAccept(**data)
+
+        assert isinstance(schema.slug, str)
+        assert str(schema.slug) == "A1b2C3d"
+
+    def test_do_not_accept_invalid_slugs(self) -> None:
+        """Invalid slugs should raise ValidationErrors"""
+        invalid_data: dict[str, str] = {"slug": "an-invalid-slug"}
+
+        with pytest.raises(ValidationError) as e:
+            schema: SlugAccept = SlugAccept(**invalid_data)
+
+        assert "validation error" in str(e.value)
+
+    def test_return_valid_short_urls(self) -> None:
+        """Application should return valid slugs"""
+        data: dict[str, str] = {"short_url": "https://jkwlsn.dev/A1b2C3d"}
+
+        schema: ShortUrlReturn = ShortUrlReturn(**data)
+
+        assert isinstance(schema.short_url, HttpUrl)
+        assert str(schema.short_url) == "https://jkwlsn.dev/A1b2C3d"
+
+    def test_do_not_return_invalid_short_urls(self) -> None:
+        """Invalid short urls should not be returned"""
+        invalid_data: dict[str, str] = {"short_url": "an-invalid-url"}
+
+        with pytest.raises(ValidationError) as e:
+            ShortUrlReturn(**invalid_data)
+
+        assert "validation error" in str(e.value)
